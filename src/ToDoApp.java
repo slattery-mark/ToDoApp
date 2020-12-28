3415206b8a6e05ef9c6c31211eeed4967efd998e
yrfriendmark <53455345+yrfriendmark@users.noreply.github.com>
2020-12-28 14:17:32 -0500

    setting up blank javafx project

---
+++ src/ToDoApp.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ToDoApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Code for application goes here
+        stage.setTitle("Hello World!");
+        stage.show();
+    }
+}
