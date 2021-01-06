c6a9f77e7c9b50ca1942c4be29ea99c926b48928
yrfriendmark <53455345+yrfriendmark@users.noreply.github.com>
2021-01-06 12:01:43 -0500

    Can update Task Name & Description from Name/Details fields

--- src/Controllers/Controller_MainScene.java
+++ src/Controllers/Controller_MainScene.java
@@ -1,23 +1,23 @@
 package Controllers;
 
 import TaskResources.Task;
-import javafx.beans.InvalidationListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Controller_MainScene implements Initializable {
 
@@ -40,8 +40,7 @@ public class Controller_MainScene implements Initializable {
             if (ListViewTasks.getSelectionModel().isEmpty()) {
                 BtnDeleteTask.setDisable(true);
                 BtnEditTask.setDisable(true);
-            }
-            else {
+            } else {
                 BtnDeleteTask.setDisable(false);
                 BtnEditTask.setDisable(false);
             }
@@ -62,6 +61,7 @@ public class Controller_MainScene implements Initializable {
         ListViewTasks.getItems().add(new Task("constructor 6", "descrip + year + month + day + hour", 2023, 4, 2, 12));
         ListViewTasks.getItems().add(new Task("constructor 7", "descrip + year + month + day + hour + minute", 2024, 5, 3, 1, 30));
     }
+
     @FXML
     void DeleteTaskBtnPress(ActionEvent event) {
         if (!ListViewTasks.getItems().isEmpty())
@@ -69,8 +69,12 @@ public class Controller_MainScene implements Initializable {
     }
 
     @FXML
-    void EditTaskBtnPress(ActionEvent event) {
-
+    void EditTaskBtnPress(ActionEvent event) throws IOException {
+        Parent editTaskSceneRoot = FXMLLoader.load(getClass().getResource("../Scenes/scene_edittask.fxml"));
+        Scene editTaskScene = new Scene(editTaskSceneRoot);
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.setScene(editTaskScene);
     }
 
     @FXML
@@ -78,42 +82,3 @@ public class Controller_MainScene implements Initializable {
 
     }
 }
-/*
-    @FXML // Launch Scene scene_newtask
-    public void NewTaskButtonPress(ActionEvent event) throws IOException {
-        // Create a Parent object (Inherits from Node object) - loads scene_newtask.fxml
-        Parent newTaskScreenRoot = FXMLLoader.load(getClass().getResource("../Scenes/scene_newtask.fxml"));
-        // Create a Scene object using Parent
-        Scene newTaskScreen = new Scene(newTaskScreenRoot);
-        // Create a Stage object
-        Stage secondaryStage = new Stage();
-        // Alter some of the Stage settings
-        secondaryStage.initModality(Modality.APPLICATION_MODAL);
-        secondaryStage.setTitle("Add a Task");
-        // Apply the Scene to the Stage
-        secondaryStage.setScene(newTaskScreen);
-        // Show the stage & wait until it is closed to execute rest of code
-        secondaryStage.showAndWait();
-        // If a task was added, get the task & add it to TaskList ListView object
-        if (secondaryStage.getUserData() != null) {
-            Task task = (Task) secondaryStage.getUserData();
-            ListTasks.getItems().add(task);
-        }
-    }
-
-    @FXML
-    public void DeleteButtonPress(ActionEvent event) throws Exception {
-        ListTasks.getItems().remove(ListTasks.getSelectionModel().getSelectedIndex());
-    }
-    @FXML
-    void TxtAreaTaskDetailsUpdate(KeyEvent event) {
-        ListTasks.getSelectionModel().getSelectedItem().setDescription(TxtAreaTaskDetails.getText());
-    }
-
-    @FXML
-    void TxtFieldTaskNameUpdate(KeyEvent event) {
-        ListTasks.getSelectionModel().getSelectedItem().setName(TxtFieldTaskName.getText());
-    }
-
-}
- */
