2e6a270c37bf9ec460a6ac4639ef495ce02c27fe
yrfriendmark <53455345+yrfriendmark@users.noreply.github.com>
2021-01-07 13:18:19 -0500

    Completed get & set methods

--- src/Controllers/Controller_EditTaskScene.java
+++ src/Controllers/Controller_EditTaskScene.java
@@ -5,16 +5,23 @@ import TaskResources.Task;
 import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller_EditTaskScene implements Initializable {
     private Task task;
+    private Scene mainScene;
     @FXML
     private Button BtnBack;
 
@@ -27,10 +34,19 @@ public class Controller_EditTaskScene implements Initializable {
     @FXML
     private Button BtnDelete;
 
+    Controller_EditTaskScene(Scene mainScene) {
+        this.mainScene = mainScene;
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         TaskName.setText(task.getName());
         TaskDescription.setText(task.getDescription());
+    }
+    @FXML
+    void BtnBackPress(ActionEvent event) throws IOException {
+        Node source = (Node)event.getSource();
+        Stage stage = (Stage)source.getScene().getWindow();
+        stage.setScene(mainScene);
     }
 
     void initData(Task task) {
