b1eec6100748e98b159c9f0e07c84cf829891c51
CJ Fox <55763324+cj-fox@users.noreply.github.com>
2021-01-07 12:16:42 -0500

    edit screen back button works.

--- src/Controllers/Controller_EditTaskScene.java
+++ src/Controllers/Controller_EditTaskScene.java
@@ -5,10 +5,15 @@ import TaskResources.Task;
 import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -35,6 +40,22 @@ public class Controller_EditTaskScene implements Initializable {
 
     void initData(Task task) {
         this.task = task;
+    }
+
+    @FXML
+    public void BtnBackPress(ActionEvent event) throws java.io.IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("../Scenes/scene_main.fxml"));
+
+        Scene scene = new Scene(root);
+        Node source = (Node)event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.setScene(scene);
+
+    }
+
+    @FXML
+    private void BtnDeletePress(ActionEvent event){
+
     }
 
 }
