30505b2dd7c3abc5da41bb4af0dd26a8e8d762ec
yrfriendmark <53455345+yrfriendmark@users.noreply.github.com>
2021-01-09 11:14:29 -0500

    Moved ToDoApp.class into Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main {
+
+    public static class ToDoApp extends Application {
+
+        @Override
+        public void init() throws Exception {
+            // Initialization, code that runs before the start i.e., loading
+        }
+
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("Scenes/scene_main.fxml"));
+            Scene mainScreen = new Scene(root);
+            primaryStage.setTitle("To Do");
+            primaryStage.setScene(mainScreen);
+            primaryStage.show();
+        }
+
+        @Override
+        public void stop() throws Exception {
+            // Code the runs after the application is stopped
+        }
+    }
 
     public static void main(String[] args) {
 
